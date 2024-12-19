aacc4554c93c8aaf9a2ddd27c40d94fb3c8ff3fc
Bekbolot <burxanovbeko@gmail.com>
2024-12-20 00:26:36 +0600

    c

--- src/components/user/Training.tsx
+++ src/components/user/Training.tsx
@@ -37,16 +37,20 @@ const Training = () => {
           <h2 className=" text-2xl md:text-3xl pb-6 text-white lg:text-[40px] font-medium mb-2">
             ЖРТ ка кантип даярдайбыз?
           </h2>
-          <p className="text-white hidden lg:block pb-4 sm:pb-16 font-medium text-lg md:text-[25px]">
+          <p className="text-white hidden lg:block pb-4 sm:pb-16  text-center text-lg md:text-[25px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            <br /> eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            eiusmod <br /> tempor incididunt ut labore et dolore magna aliqua.
           </p>
         </div>
 
-        <div className="flex flex-col lg:flex-row items-center justify-between gap-0 lg:gap-3">
-          {training.map((item) => (
-            <React.Fragment key={item.id}>
-              <div className="flex flex-col items-center space-y-4">
+        <div className="flex flex-col  lg:flex-row items-center justify-between gap-0 lg:gap-3">
+          {/* Мобильная версия */}
+          <div className="lg:hidden">
+            {training.map((item) => (
+              <div
+                key={item.id}
+                className="flex flex-col items-center space-y-5 pb-10"
+              >
                 <div className="flex justify-center">
                   <Image
                     src={item.imgSrc}
@@ -61,20 +65,54 @@ const Training = () => {
                 <h4 className="text-white text-[18px] text-center">
                   {item.description}
                 </h4>
-              </div>
 
-              {item.id !== 3 && (
-                <div className="rotate-90 lg:rotate-0">
-                  <Image
-                    src="/images/Line.svg"
-                    alt="divider"
-                    width={2}
-                    height={249}
-                  />
+                {item.id !== 3 && (
+                  <div
+                    className="w-[390px]  h-[2px] mx-auto"
+                    style={{
+                      backgroundImage:
+                        "linear-gradient(to right, white 50%, transparent 50%)",
+                      backgroundSize: "10px 2px",
+                    }}
+                  ></div>
+                )}
+              </div>
+            ))}
+          </div>
+
+          <div className="hidden lg:flex flex-row items-center justify-between gap-3">
+            {training.map((item) => (
+              <React.Fragment key={item.id}>
+                <div className="flex flex-col items-center space-y-4">
+                  <div className="flex justify-center">
+                    <Image
+                      src={item.imgSrc}
+                      alt={item.title}
+                      width={94}
+                      height={71}
+                    />
+                  </div>
+                  <p className="text-white font-extrabold text-[25px] text-center">
+                    {item.title}
+                  </p>
+                  <h4 className="text-white text-[18px] text-center">
+                    {item.description}
+                  </h4>
                 </div>
-              )}
-            </React.Fragment>
-          ))}
+
+                {item.id !== 3 && (
+                  <div className="rotate-90 lg:rotate-0">
+                    <Image
+                      src="/images/Line.svg"
+                      alt="divider"
+                      width={2}
+                      height={249}
+                    />
+                  </div>
+                )}
+              </React.Fragment>
+            ))}
+          </div>
         </div>
       </div>
     </div>
