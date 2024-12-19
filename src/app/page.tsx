a88bdf29a22db556b478da5f9e9c7a6a544f2e15
muxammed0405 <muhammednazardev@gmail.com>
2024-12-20 00:40:32 +0600

    installed shad/cn also input now you can just use input of shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+/** @format */
+
+import { inputStyles } from "@/components/UI/CustomInputClassName"
+import { Input } from "@/components/UI/input"
+
 export default function Home() {
-  return <div></div>;
+	return (
+		<div>
+			<Input type="text" placeholder="text" className={inputStyles} />
+		</div>
+	)
 }
