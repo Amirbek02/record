7aa4b87d9d46602c6f43cca8510e2883068275f1
Bekbolot <burxanovbeko@gmail.com>
2024-12-19 19:18:09 +0600

    c

--- src/components/user/Testlist.tsx
+++ src/components/user/Testlist.tsx
@@ -13,8 +13,9 @@ const TestPage = () => {
     {
       id: 1,
       title: "Кыргыз тил",
-      description: "Кыргыз тил боюнча негизги тестке кош келиңиз",
-      tag: "Кыргыз тил",
+      description:
+        "30 тапшырмадан турат, аны аткарууга 30 мүнөт убакыт берилет. Тапшырманы аткарып жатып, тандаган жообуңарды өзгөртсөңөр болот, бирок жоопту ар бир тапшырмада бир гана жолу өзгөртө аласыз!",
+      tag: "Кыргыз тил боюнча негизги тестке кош келиңиз",
       imgSrc: "/images/test.jpg",
     },
     {
@@ -22,7 +23,7 @@ const TestPage = () => {
       title: "Математика",
       tag: "Математика боюнча даярдалган видео сабакка кош келиңиз",
       description:
-        "30 Видео сабак 4 тестик суроо камтылган. Сабакты көрүп бүткөндөн кийин...",
+        "30 тапшырмадан турат, аны аткарууга 30 мүнөт убакыт берилет. Тапшырманы аткарып жатып, тандаган жообуңарды өзгөртсөңөр болот, бирок жоопту ар бир тапшырмада бир гана жолу өзгөртө аласыз!",
       imgSrc: "/images/test.jpg",
     },
   ];
@@ -35,27 +36,62 @@ const TestPage = () => {
         </h2>
       </div>
 
-      <div>
-        <p
-          className={`${montserrat.className}text-xl font-bold text-[#4C4C4C] lg:text-[32px] mb-4`}
-        >
+      <div className="hidden sm:block">
+        <p className="text-xl font-bold text-[#4C4C4C] lg:text-[32px] mb-4">
           Негизги тест
         </p>
-        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="grid lg:grid-cols-2 gap-6">
           {test.map((testi) => (
-            <div key={testi.id} className="space-y-4 text-center">
-              <Image
-                src={testi.imgSrc}
-                width={605}
-                height={345}
-                alt="img"
-                className="mx-auto"
-              />
-              <h3 className="text-lg  font-medium">{testi.title}</h3>
-              <p className="text-sm">{testi.description}</p>
+            <div key={testi.id} className="space-y-4 text-start flex flex-col">
+              <div className="relative">
+                <Image
+                  src={testi.imgSrc}
+                  width={605}
+                  height={345}
+                  alt="img"
+                  className="mx-auto"
+                />
+                <h3
+                  className="absolute text-center w-64 border-black rounded-xl cursor-pointer p-2 bg-[#4C4C4C] hover:bg-[#2E3095] text-2xl font-medium text-white 
+                  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                >
+                  {testi.title}
+                </h3>
+              </div>
+
+              <div className="flex flex-col  mt-auto">
+                <p className="text-2xl font-medium">{testi.tag}</p>
+                <p className="text-sm opacity-50">{testi.description}</p>
+                <b className="underline  cursor-pointer">Толук оку</b>
+              </div>
             </div>
           ))}
         </div>
+      </div>
+
+      <div className="block sm:hidden">
+        <p className="text-xl font-bold text-[#4C4C4C] mb-4">Мобильный тест</p>
+        {test.map((testi) => (
+          <div key={testi.id} className="space-y-4 text-start">
+            <div className="relative w-[356px]">
+              <Image
+                src={testi.imgSrc}
+                width={305}
+                height={195}
+                alt="img"
+                className="mx-auto"
+              />
+              <h3 className="absolute text-center w-32 border-black rounded-xl cursor-pointer p-2 bg-[#4C4C4C] hover:bg-[#2E3095] bottom-2 right-9 transform text-sm font-medium text-white">
+                {testi.title}
+              </h3>
+            </div>
+            <div className="flex flex-col">
+              <p className="text-lg font-medium">{testi.tag}</p>
+              <p className="text-sm opacity-30">{testi.description}</p>
+              <b className="underline pb-5 cursor-pointer ">Толук оку</b>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
