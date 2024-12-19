c172807782c8f647bf59fd897b6c3ef412aa99c8
Alisherov017 <r.alisherov2005@gmail.com>
2024-12-19 21:46:57 +0600

    fixed some bugs

--- src/components/UI/CustomButton.tsx
+++ src/components/UI/CustomButton.tsx
@@ -4,19 +4,25 @@ export interface CustomButtonProps {
   title?: string;
   containerStyles?: string;
   textStyles?: string;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
 }
 
 const CustomButton = ({
   title,
-  containerStyles,
-  textStyles,
+  containerStyles = "",
+  textStyles = "",
+  type = "button",
+  onClick,
 }: CustomButtonProps) => {
   return (
     <button
+      type={type}
+      onClick={onClick}
       className={`rounded-[80px] bg-[rgb(46,48,149)] box-border w-[160px] h-[56px] ${containerStyles}`}
     >
       <span
-        className={`font-[500px] text-[22px] leading-[27px] font-Montserrat  ${textStyles} `}
+        className={`font-[500px] text-[22px] leading-[27px] font-Montserrat ${textStyles}`}
       >
         {title}
       </span>
