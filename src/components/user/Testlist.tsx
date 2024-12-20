d9e563a594b317878d96dd53eb15bf3c5ca649d2
Bekbolot <burxanovbeko@gmail.com>
2024-12-20 17:44:16 +0600

    testlist

--- src/components/user/Testlist.tsx
+++ src/components/user/Testlist.tsx
@@ -1,13 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { Montserrat } from "next/font/google";
-
-const montserrat = Montserrat({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
-});
-
+import Link from "next/link";
 const TestPage = () => {
   const test = [
     {
@@ -29,15 +23,15 @@ const TestPage = () => {
   ];
 
   return (
-    <div className={`${montserrat.className} max-w-6xl mx-auto p-4`}>
-      <div className="text-center mb-8">
-        <h2 className="text-xl md:text-2xl text-[#4C4C4C] lg:text-[36px] font-medium mb-2">
+    <div className=" max-w-6xl mx-auto p-4">
+      <div className="text-center mb-3 lg:mb-8">
+        <h2 className="text-xl md:text-2xl text-[#4C4C4C] lg:text-[36px] font-medium ">
           Сынамык тестке кош келиңиз!
         </h2>
       </div>
 
       <div className="hidden sm:block">
-        <p className="text-xl font-bold text-[#4C4C4C] lg:text-[32px] mb-4">
+        <p className="text-xl lg:text-start md:text-center font-bold text-[#4C4C4C] lg:text-[32px] mb-4">
           Негизги тест
         </p>
         <div className="grid lg:grid-cols-2 gap-6">
@@ -62,7 +56,9 @@ const TestPage = () => {
               <div className="flex flex-col  mt-auto">
                 <p className="text-2xl font-medium">{testi.tag}</p>
                 <p className="text-sm opacity-50">{testi.description}</p>
-                <b className="underline  cursor-pointer">Толук оку</b>
+                <Link href="/" className="underline font-bold text-[20px]  ">
+                  Толук оку
+                </Link>
               </div>
             </div>
           ))}
@@ -70,25 +66,27 @@ const TestPage = () => {
       </div>
 
       <div className="block sm:hidden">
-        <p className="text-xl font-bold text-[#4C4C4C] mb-4">Мобильный тест</p>
+        <p className="text-xl font-medium text-[#4C4C4C] mb-4">Негизги тест</p>
         {test.map((testi) => (
           <div key={testi.id} className="space-y-4 text-start">
-            <div className="relative w-[356px]">
+            <div className="relative w-[339px]">
               <Image
                 src={testi.imgSrc}
-                width={305}
+                width={365}
                 height={195}
                 alt="img"
                 className="mx-auto"
               />
-              <h3 className="absolute text-center w-32 border-black rounded-xl cursor-pointer p-2 bg-[#4C4C4C] hover:bg-[#2E3095] bottom-2 right-9 transform text-sm font-medium text-white">
+              <h3 className="absolute text-center w-32 border-black rounded-xl cursor-pointer p-2 bg-[#4C4C4C] hover:bg-[#2E3095] bottom-2 right-3 transform text-sm font-medium text-white">
                 {testi.title}
               </h3>
             </div>
             <div className="flex flex-col">
               <p className="text-lg font-medium">{testi.tag}</p>
               <p className="text-sm opacity-30">{testi.description}</p>
-              <b className="underline pb-5 cursor-pointer ">Толук оку</b>
+              <Link href="/" className="underline pb-5 font-bold text-[14px] ">
+                Толук оку
+              </Link>
             </div>
           </div>
         ))}
