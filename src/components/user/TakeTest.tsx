f613721f2b8804900c16fec2f286a354febc5e15
Alisherov017 <r.alisherov2005@gmail.com>
2024-12-23 20:21:16 +0600

    feature/test1 is finished

--- src/components/user/TakeTest.tsx
+++ src/components/user/TakeTest.tsx
@@ -25,22 +25,26 @@ const TakeTest = () => {
           <h2 className="font-montserrat text-[#4c4c4c] text-[20px] font-[500] leading-[80%] tracking-[0.2px] pb-[24px] lg:hidden">
             1-бөлүм. Математика
           </h2>
-          <p className="font-montserrat text-[#4c4c4c] text-[20px] font-[500] leading-[80%] tracking-[0.2px] lg:hidden">
+          <p className="font-montserrat text-[#4c4c4c] text-[28px] font-[700] leading-[80%] tracking-[0.2px] hidden lg:block">
             Негизги тест
           </p>
-          <p className="text-[rgb(76,76,76)] font-montserrat text-[16px] font-[500] leading-[28px]">
+          <p className="hidden lg:block font-montserrat text-[#4c4c4c] text-[24px] font-[500] leading-[80%] tracking-[0.2px] pt-[16px] pb-[22px]">
+            Математика боюнча предметтик сынамык тест.
+          </p>
+
+          <p className="text-[rgb(76,76,76)] font-montserrat text-[16px] lg:text-[24px] font-[500] leading-[28px] lg:pb-[10px] ">
             Тест{" "}
             <span className="text-[rgb(240,26,38)] font-[700]">
               30 тапшырмадан
             </span>{" "}
             турат.
           </p>
-          <p className="text-[rgb(76,76,76)] font-montserrat text-[16px] font-[500] leading-[28px]">
+          <p className="text-[rgb(76,76,76)] font-montserrat text-[16px] lg:text-[24px] font-[500] leading-[28px] lg:pb-[8px]">
             Аны аткарууга{" "}
             <span className="text-[rgb(240,26,38)] font-[700]">30 мүнөт</span>{" "}
             убакыт берилет.
           </p>
-          <p className="text-[rgb(76,76,76)] font-montserrat text-[16px] font-[500] leading-[28px]">
+          <p className="text-[rgb(76,76,76)] font-montserrat text-[16px] lg:text-[24px] font-[500] leading-[28px]  lg:leading-[42px]  ">
             Тапшырманы аткарып жатып, тандаган жообуңарды{" "}
             <span className=" text-[rgb(0,131,53)] font-[700]">
               өзгөртсөңөр болот
@@ -51,7 +55,7 @@ const TakeTest = () => {
             </span>{" "}
             өзгөртө аласыз.
           </p>
-          <p className="text-[rgb(76,76,76)] font-montserrat text-[16px] font-[500] leading-[28px]">
+          <p className="text-[rgb(76,76,76)] font-montserrat text-[16px] lg:text-[24px] font-[500] leading-[28px] lg:leading-[42px]">
             Оңдой турган жаңы (туура) жоопту{" "}
             <span className="text-[rgb(0,131,53)] font-[700]">
               чарчы кылып боёшуңуз керек.
@@ -65,13 +69,13 @@ const TakeTest = () => {
             </span>{" "}
             аласыз.
           </p>
-          <h3 className="text-[rgb(0,131,53)] font-montserrat pt-[24px] text-[24px] font-[700] leading-[28px]">
+          <h3 className="text-[rgb(0,131,53)] lg:text-[rgb(240,26,38)] font-montserrat pt-[24px] text-[24px] font-[700] leading-[28px]">
             Ийгилик каалайбыз!
           </h3>
         </div>
         <CustomButton
           title="Тестти баштоо"
-          containerStyles="h-[40px] small:h-[50px] w-[220px] small:w-[360px] rounded-[10px]"
+          containerStyles="h-[40px] small:h-[50px] w-[220px] lg:w-[185px] small:w-[360px] rounded-[10px] lg:self-end "
           textStyles="font-[500] small:font-[700] text-[18px] small:text-[20px] leading-[30px] font-poppins text-[rgb(255,255,255)]"
         />
       </div>
