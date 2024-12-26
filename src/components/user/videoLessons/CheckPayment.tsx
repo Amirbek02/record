82f828168c937dff87f1ad46ace152d3a86e49e3
janettadev <kumarbekovajanetta@gmail.com>
2024-12-26 21:54:37 +0600

    added video lessons before payment

---
+++ src/components/user/videoLessons/CheckPayment.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import smile from "../../../../public/images/smile.svg";
+import Image from "next/image";
+
+const CheckPayment = () => {
+	return (
+		<div className="w-[90%] max-w-[1440px] mx-auto py-[30px]">
+			<h1 className=" hidden md:block md:text-[24px] font-semibold md:font-bold tracking-[2%] text-[#4C4C4C] mb-[15px] md:mb-[24px]">
+				1- бөлүм. Математика
+			</h1>
+			<div className="flex flex-col">
+				<h2 className="text-center font-bold text-[24px] md:text-[32px] md:font-medium leading-[130%] text-[#4C4C4C] order-2">
+					2-видео сабак көрүү үчүн төлөм аткарыңыз!
+				</h2>
+				<Image
+					src={smile}
+					alt=""
+					width={150}
+					height={150}
+					className="mx-auto mt-[70px] order-1"
+				/>
+			</div>
+			<div className="flex flex-col gap-[8px] items-center mt-[80px]">
+				<button className="font-semibold text-[20px] py-[10px] px-[100px] w-full md:w-[422px] rounded-[5px] text-[#FFFFFF] bg-blue-600">
+					Төлөм аткаруу
+				</button>
+				<button className="font-semibold text-[20px] py-[10px] px-[100px] w-full md:w-[422px] rounded-[5px] text-[#4C4C4C] bg-[#EAEAEA]">
+					Аяктоо
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default CheckPayment;
---
+++ src/components/user/videoLessons/VideoPlayer.tsx
@@ -0,0 +1,53 @@
+"use client";
+// before payment
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+
+const VideoPlayer = () => {
+	const [play, setPlay] = useState(false);
+
+	return (
+		<div className="w-[90%] max-w-[1440px] mx-auto py-[30px]">
+			<h1 className="text-[18px] md:text-[24px] font-semibold md:font-bold tracking-[2%] text-[#4C4C4C] mb-[15px] md:mb-[24px]">
+				1- бөлүм. Математика
+			</h1>
+			<div>
+				<div className="w-full h-auto md:w-[734px] lg:w-[834px] lg:h-[502px] rounded-[15px] overflow-hidden">
+					<div
+						className={`relative rounded-[15px] overflow-hidden border-[8px] transition-all duration-500 ease-in-out md:border-[2px] md:border-[#008335] ${
+							play ? "border-[#2E3095]" : "border-0"
+						}`}>
+						<ReactPlayer
+							url="/video/nature.mp4"
+							controls
+							width="100%"
+							height="100%"
+							className="rounded-[15px]"
+							onPlay={() => setPlay(false)}
+							onPause={() => setPlay(true)}
+						/>
+						<div
+							className={`transition-all duration-500 ease-in-out ${
+								play ? "flex" : "hidden"
+							} md:hidden w-[127px] h-[26px] flex items-center justify-center rounded-[80px] bg-blue-600 absolute bottom-[15px] right-[15px]`}>
+							<h3 className=" ">Математика</h3>
+						</div>
+					</div>
+				</div>
+				<h2 className="text-[16px] md:text-[20px] font-semibold md:font-medium ">
+					Математика боюнча 1 - видео сабакка кош келиңиз!
+				</h2>
+				<div className="flex gap-[10px] mt-[100px] md:mt-[70px] justify-end">
+					<button className="hidden md:block w-[149px]  px-[15px] py-[10px] text-[#4C4C4C] bg-[#D0D0D0] rounded-[5px] font-medium text-[20px]">
+						Артка
+					</button>
+					<button className="w-[320px] md:w-[149px] px-[15px] py-[10px] text-[#fff] bg-blue-600 rounded-[5px] font-medium text-[20px]">
+						Алдыга
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default VideoPlayer;
