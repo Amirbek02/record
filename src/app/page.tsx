8f45f755ca684755ca6d5c934aca4435a0181438
Alisherov017 <r.alisherov2005@gmail.com>
2025-01-03 19:33:15 +0600

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import SignIn from "@/components/user/auth/SignIn";
-// import SignUp from "@/components/user/auth/SignUp";
-
 export default function Home() {
-  return (
-    <div>
-      {/* <SignUp /> */}
-      <SignIn />
-    </div>
-  );
+  return <div></div>;
 }
