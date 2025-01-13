1b554295726075e7865c001c7fa1ffadee2cf04f
Bekbolot <burxanovbeko@gmail.com>
2025-01-13 15:00:03 +0600

    added page commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import PaidVideoLessonTest from "@/components/user/videoLessons/PaidVideoLessons/PaidVideoLessonTest";
-import PaidVideoPlayer from "@/components/user/videoLessons/PaidVideoLessons/PaidVideoPlayer";
-
 export default function Home() {
-  return (
-    <div>
-      <PaidVideoLessonTest />
-      <PaidVideoPlayer />
-    </div>
-  );
+  return <div></div>;
 }
