cb66f5d949ca4bb1e2dd41c0a847a1f00b2693c1
Amirbek02 <amirbek.amiraev.kk@gmail.com>
2025-01-19 02:59:41 +0600

    fix2

--- src/app/in/payment/[...slug]/page.tsx
+++ src/app/in/payment/[...slug]/page.tsx
@@ -1,11 +1,17 @@
-import React from "react";
-import MakePayment from "@/components/user/payments/MakePayment";
-import AcceptPayment from "@/components/user/payments/AcceptPayment";
+import React from 'react';
+import MakePayment from '@/components/user/payments/MakePayment';
+import AcceptPayment from '@/components/user/payments/AcceptPayment';
 
 const page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const slugDepth = (await params).slug?.length;
   return (
-    <section>{slugDepth === 1 ? <MakePayment /> : <AcceptPayment />}</section>
+    <section>
+      {slugDepth === 1 ? (
+        <MakePayment telNumber={'+996777380432'} fullName={'Амирбек Амираев'} amount={23} />
+      ) : (
+        <AcceptPayment />
+      )}
+    </section>
   );
 };
 
