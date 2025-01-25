a2eb848cd56cecd5ef87072dd3793e8f8c81c7b3
Amirbek02 <amirbek.amiraev.kk@gmail.com>
2025-01-25 16:00:59 +0600

    add router, checked localstore

--- src/components/user/auth/SignUp.tsx
+++ src/components/user/auth/SignUp.tsx
@@ -1,172 +1,168 @@
-"use client";
-import CustomButton from "@/components/UI/CustomButton";
-import useAuthStore from "@/lib/store/authStore";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
-import { FaRegEyeSlash, FaRegEye } from "react-icons/fa";
+'use client';
+import CustomButton from '@/components/UI/CustomButton';
+import useAuthStore from '@/lib/store/authStore';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { FaRegEyeSlash, FaRegEye } from 'react-icons/fa';
 
 const SignUp = () => {
-	const { login, isLoading, error } = useAuthStore();
-	const [showPassword, setShowPassword] = useState(false);
-	const [isChecked, setIsChecked] = useState(false);
-	const [formData, setFormData] = useState({
-		email: "",
-		password: "",
-	});
+  const { login, error } = useAuthStore();
+  const [showPassword, setShowPassword] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target;
-		setFormData((prevData) => ({ ...prevData, [name]: value }));
-	};
+  React.useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      loginWithToken(token);
+    }
+  }, []);
 
-	const handleSignIn = async (e: React.MouseEvent<HTMLButtonElement>) => {
-		e.preventDefault();
-		const { email, password } = formData;
-		try {
-			await login(email, password);
-		} catch (err) {
-			console.error("Error during login:", err);
-			alert(error || "Failed to log in. Please try again.");
-		}
-	};
+  const loginWithToken = async (token: string) => {
+    try {
+      await login('', token);
+      router.push('/in');
+    } catch (err) {
+      console.error('Error during token login:', err);
+    }
+  };
 
-	const inputClass =
-		"font-montserrat font-[400] box-border border-[0.6px] border-gray-300 placeholder-gray-300 text-[rgba(0,0,0,0.44)] w-[270px] mm:w-[360px] xl:w-[500px] rounded-[4px] py-[19px] pl-[15px] mb-[32px] text-[16px] leading-[20px]";
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-	return (
-		<div className="flex justify-center items-center flex-col w-[360px] mm:w-auto">
-			<div className="pt-[26px] xl:pt-[120px] pb-[82px] xl:pb-[40px]">
-				<Image
-					src="/icons/record-logo.svg"
-					width={290}
-					height={95}
-					alt="Sign In"
-				/>
-			</div>
-			<h1 className="hidden xl:block font-montserrat font-medium text-[36px] leading-[44px] tracking-[2%] text-[rgb(76,76,76)] pb-[75px]">
-				Кош келдиңиз!
-			</h1>
+  const handleSignIn = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const { email, password } = formData;
+    try {
+      await login(email, password);
+      router.push('/in');
+    } catch (err) {
+      console.error('Error during login:', err);
+      alert(error || 'Failed to log in. Please try again.');
+    }
+  };
 
-			<form className="flex justify-center items-center flex-col relative">
-				<div className="flex flex-col items-start">
-					<label
-						htmlFor="email"
-						className="font-montserrat font-[400] text-[16px] leading-[20px] pb-[6px] text-[rgb(76,76,76)]">
-						Телефон номери
-					</label>
-					<input
-						type="email"
-						id="email"
-						name="email"
-						placeholder="qwerty@gmail.com"
-						value={formData.email}
-						onChange={handleInputChange}
-						className={inputClass}
-					/>
-				</div>
+  const inputClass =
+    'font-montserrat font-[400] box-border border-[0.6px] border-gray-300 placeholder-gray-300 text-[rgba(0,0,0,0.44)] w-[270px] mm:w-[360px] xl:w-[500px] rounded-[4px] py-[19px] pl-[15px] mb-[32px] text-[16px] leading-[20px]';
 
-				<div className="flex flex-col items-start">
-					<label
-						htmlFor="password"
-						className="font-montserrat font-[400] text-[16px] leading-[20px] pb-[6px] text-[rgb(76,76,76)]">
-						Сыр сөз
-					</label>
-					<div className="relative w-[270px] mm:w-[356px] xl:w-[500px]">
-						<input
-							type={showPassword ? "text" : "password"}
-							id="password"
-							name="password"
-							placeholder="**********"
-							value={formData.password}
-							onChange={handleInputChange}
-							className={inputClass}
-						/>
-						<button
-							type="button"
-							onClick={() => setShowPassword(!showPassword)}
-							className="absolute top-[30px] right-[25px] transform -translate-y-1/2 text-[rgba(0,0,0,0.44)]">
-							{showPassword ? (
-								<FaRegEyeSlash size={20} />
-							) : (
-								<FaRegEye size={20} />
-							)}
-						</button>
-					</div>
-				</div>
+  return (
+    <div className="flex justify-center items-center flex-col w-[360px] mm:w-auto">
+      <div className="pt-[26px] xl:pt-[120px] pb-[82px] xl:pb-[40px]">
+        <Image src="/icons/record-logo.svg" width={290} height={95} alt="Sign In" />
+      </div>
+      <h1 className="hidden xl:block font-montserrat font-medium text-[36px] leading-[44px] tracking-[2%] text-[rgb(76,76,76)] pb-[75px]">
+        Кош келдиңиз!
+      </h1>
 
-				<div className="flex justify-between items-center w-[270px] mm:w-[360px] xl:w-[500px] pb-[32px]">
-					<label className="flex items-center">
-						<input
-							type="checkbox"
-							checked={isChecked}
-							onChange={() => setIsChecked(!isChecked)}
-							className="hidden"
-						/>
-						<div className="w-[20px] h-[20px] border rounded-[4px] flex items-center justify-center">
-							{isChecked && (
-								<span className="text-[14px] font-bold text-[rgba(0,0,0,0.6)]">
-									✔
-								</span>
-							)}
-						</div>
-						<span className="font-roboto text-[14px] font-[400] leading-[16px] text-[rgba(0,0,0,0.6)] pl-[17px]">
-							Эстеп кал
-						</span>
-					</label>
-					<Link
-						href="#"
-						className="font-roboto text-[14px] font-[400] leading-[16px] text-[rgb(46,48,149)] underline">
-						Сыр сөзүңүздү унуттуңузбу?
-					</Link>
-				</div>
+      <form className="flex justify-center items-center flex-col relative">
+        <div className="flex flex-col items-start">
+          <label
+            htmlFor="email"
+            className="font-montserrat font-[400] text-[16px] leading-[20px] pb-[6px] text-[rgb(76,76,76)]">
+            Телефон номери
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="qwerty@gmail.com"
+            value={formData.email}
+            onChange={handleInputChange}
+            className={inputClass}
+          />
+        </div>
 
-				<CustomButton
-					title="Кирүү"
-					containerStyles="rounded-[5px] h-[45px] mm:h-[56px] mm:w-[356px] xl:w-[500px] mb-[32px]"
-					textStyles="font-[500] mm:font-[700] text-[21px] mm:text-[24px] leading-[29px] text-[rgb(255,255,255)]"
-					onClick={handleSignIn}
-				/>
+        <div className="flex flex-col items-start">
+          <label
+            htmlFor="password"
+            className="font-montserrat font-[400] text-[16px] leading-[20px] pb-[6px] text-[rgb(76,76,76)]">
+            Сыр сөз
+          </label>
+          <div className="relative w-[270px] mm:w-[356px] xl:w-[500px]">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              id="password"
+              name="password"
+              placeholder="**********"
+              value={formData.password}
+              onChange={handleInputChange}
+              className={inputClass}
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute top-[30px] right-[25px] transform -translate-y-1/2 text-[rgba(0,0,0,0.44)]">
+              {showPassword ? <FaRegEyeSlash size={20} /> : <FaRegEye size={20} />}
+            </button>
+          </div>
+        </div>
 
-				<div className="flex flex-col items-center pb-[175px]">
-					<div className="flex items-center text-[rgb(163,163,174)] font-montserrat text-[16px] font-[500] leading-[20px] pb-[32px]">
-						<span className="flex-1 border-t border-[rgb(163,163,174)]"></span>
-						<p className="px-[16px]">Же</p>
-						<span className="flex-1 border-t border-[rgb(163,163,174)]"></span>
-					</div>
-					<div className="flex justify-between w-[270px] mm:w-[360px] xl:w-[500px] pb-[32px]">
-						<button className="flex items-center border-[0.6px] border-[rgb(228,228,228)] rounded-[4px] px-[10px] py-[15px]">
-							<Image
-								src="/icons/iphone.svg"
-								width={24}
-								height={24}
-								alt="iCloud"
-							/>
-							<span className="pl-[10px] text-[rgb(163,163,174)] text-[16px] font-[400]">
-								icloud аркылуу
-							</span>
-						</button>
-						<button className="flex items-center border-[0.6px] border-[rgb(228,228,228)] rounded-[4px] px-[10px] py-[15px]">
-							<Image
-								src="/icons/google.svg"
-								width={24}
-								height={24}
-								alt="Google"
-							/>
-							<span className="pl-[10px] text-[rgb(163,163,174)] text-[16px] font-[400]">
-								Google аркылуу
-							</span>
-						</button>
-					</div>
-					<Link
-						href="#"
-						className="text-[rgb(46,48,149)] font-roboto text-[20px] font-normal underline">
-						Катталуу
-					</Link>
-				</div>
-			</form>
-		</div>
-	);
+        <div className="flex justify-between items-center w-[270px] mm:w-[360px] xl:w-[500px] pb-[32px]">
+          <label className="flex items-center">
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={() => setIsChecked(!isChecked)}
+              className="hidden"
+            />
+            <div className="w-[20px] h-[20px] border rounded-[4px] flex items-center justify-center">
+              {isChecked && <span className="text-[14px] font-bold text-[rgba(0,0,0,0.6)]">✔</span>}
+            </div>
+            <span className="font-roboto text-[14px] font-[400] leading-[16px] text-[rgba(0,0,0,0.6)] pl-[17px]">
+              Эстеп кал
+            </span>
+          </label>
+          <Link
+            href="#"
+            className="font-roboto text-[14px] font-[400] leading-[16px] text-[rgb(46,48,149)] underline">
+            Сыр сөзүңүздү унуттуңузбу?
+          </Link>
+        </div>
+
+        <CustomButton
+          title="Кирүү"
+          containerStyles="rounded-[5px] h-[45px] mm:h-[56px] mm:w-[356px] xl:w-[500px] mb-[32px]"
+          textStyles="font-[500] mm:font-[700] text-[21px] mm:text-[24px] leading-[29px] text-[rgb(255,255,255)]"
+          onClick={handleSignIn}
+        />
+
+        <div className="flex flex-col items-center pb-[175px]">
+          <div className="flex items-center text-[rgb(163,163,174)] font-montserrat text-[16px] font-[500] leading-[20px] pb-[32px]">
+            <span className="flex-1 border-t border-[rgb(163,163,174)]"></span>
+            <p className="px-[16px]">Же</p>
+            <span className="flex-1 border-t border-[rgb(163,163,174)]"></span>
+          </div>
+          <div className="flex justify-between w-[270px] mm:w-[360px] xl:w-[500px] pb-[32px]">
+            <button className="flex items-center border-[0.6px] border-[rgb(228,228,228)] rounded-[4px] px-[10px] py-[15px]">
+              <Image src="/icons/iphone.svg" width={24} height={24} alt="iCloud" />
+              <span className="pl-[10px] text-[rgb(163,163,174)] text-[16px] font-[400]">
+                icloud аркылуу
+              </span>
+            </button>
+            <button className="flex items-center border-[0.6px] border-[rgb(228,228,228)] rounded-[4px] px-[10px] py-[15px]">
+              <Image src="/icons/google.svg" width={24} height={24} alt="Google" />
+              <span className="pl-[10px] text-[rgb(163,163,174)] text-[16px] font-[400]">
+                Google аркылуу
+              </span>
+            </button>
+          </div>
+          <Link
+            href="#"
+            className="text-[rgb(46,48,149)] font-roboto text-[20px] font-normal underline">
+            Катталуу
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
--- src/lib/store/authStore.ts
+++ src/lib/store/authStore.ts
@@ -1,83 +1,72 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
-import axios from "axios";
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import axios from 'axios';
 
 export interface IFormData {
-	name: string;
-	lastName: string;
-	email: string;
-	password: string;
-	confirmPassword: string;
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 }
 
 interface AuthState {
-	isAuthenticated: boolean;
-	user: any | null;
-	token: string | null;
-	isLoading: boolean;
-	error: string | null;
-	successMessage: string | null;
-	login: (email: string, password: string) => Promise<void>;
-	// register: (formData: IFormData) => Promise<void>;
-	// logout: () => void;
-	clearMessages: () => void;
+  isAuthenticated: boolean;
+  user: unknown | null;
+  token: string | null;
+  isLoading: boolean;
+  error: string | null;
+  successMessage: string | null;
+  login: (email: string, password: string) => Promise<void>;
+  clearMessages: () => void;
 }
 
-const apiClient = axios.create({
-	baseURL: "https://api.recordonline.kg/api/v1",
-	headers: { "Content-Type": "application/json" },
-});
-
-const validateFormData = (formData: IFormData): string | null => {
-	if (!formData.email.includes("@")) return "Invalid email format.";
-	if (formData.password !== formData.confirmPassword)
-		return "Passwords do not match.";
-	if (!formData.name || !formData.lastName)
-		return "Name fields cannot be empty.";
-	return null;
-};
-
 const useAuthStore = create<AuthState>()(
-	persist(
-		(set) => ({
-			isAuthenticated: false,
-			user: null,
-			token: null,
-			isLoading: false,
-			error: null,
-			successMessage: null,
+  persist(
+    (set) => ({
+      isAuthenticated: false,
+      user: null,
+      token: null,
+      isLoading: false,
+      error: null,
+      successMessage: null,
 
-			login: async (email, password) => {
-				set({ isLoading: true, error: null });
+      login: async (email, password) => {
+        set({ isLoading: true, error: null });
 
-				try {
-					const { data } = await axios.post(
-						"https://api.recordonline.kg/api/v1/sign-in/",
-						{ email, password },
-						{ headers: { "Content-Type": "application/json" } }
-					);
+        try {
+          const { data } = await axios.post(
+            'https://api.recordonline.kg/api/v1/sign-in/',
+            { email, password },
+            { headers: { 'Content-Type': 'application/json' } },
+          );
 
-					set({
-						token: data.token,
-						user: data.user,
-						isAuthenticated: true,
-					});
-					localStorage.setItem("token", data.token);
-					console.log("Login successful:", data.token);
-				} catch (error: any) {
-					set({ error: error.response?.data?.message || "Login failed" });
-					console.error("Login error:", error.response?.data);
-				} finally {
-					set({ isLoading: false });
-				}
-			},
+          set({
+            token: data.token,
+            user: data.user,
+            isAuthenticated: true,
+          });
+          localStorage.setItem('token', data.token);
+          console.log('Кирүү ийгиликтүү аткарылды:', data.token);
+        } catch (error: unknown) {
+          if (axios.isAxiosError(error)) {
+            set({ error: error.response?.data?.message || 'Кирүү мүмкүн эмес' });
+            console.error('Кирүүдө ката кетти:', error.response?.data);
+          } else {
+            set({ error: 'Кирүү мүмкүн эмес' });
+            console.error('Кирүүдө ката кетти:', error);
+          }
+        } finally {
+          set({ isLoading: false });
+        }
+      },
 
-			clearMessages: () => set({ error: null, successMessage: null }),
-		}),
-		{
-			name: "auth",
-		}
-	)
+      clearMessages: () => set({ error: null, successMessage: null }),
+    }),
+    {
+      name: 'auth',
+    },
+  ),
 );
 
 export default useAuthStore;
