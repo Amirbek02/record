206eaac7cdc8adaf8799c40c42bd3edea1bb5083
Amirbek02 <amirbek.amiraev.kk@gmail.com>
2025-01-27 15:10:45 +0600

    added contact, fixed mistakes

--- src/components/user/Lesson.tsx
+++ src/components/user/Lesson.tsx
@@ -20,6 +20,16 @@ const VideoLessons = () => {
 
   const freeVideos = videos.filter((video) => video.is_paid === false);
 
+  const extractVideoId = (videoUrl: string) => {
+    try {
+      const url = new URL(videoUrl);
+      return new URLSearchParams(url.search).get('v');
+    } catch (e) {
+      console.error('Ошибка при обработке URL видео:', e);
+      return null;
+    }
+  };
+
   return (
     <div className="max-w-6xl mx-auto p-4" id="videoLessons">
       <div className="text-center mb-8">
@@ -35,71 +45,39 @@ const VideoLessons = () => {
         {freeVideos.length > 0 && (
           <div className="space-y-4 max-w-[800px]">
             <div className="relative w-full h-auto pb-[56.25%] ">
-              <iframe
-                className="absolute top-0 left-0 w-full h-full rounded-xl border"
-                src={`https://www.youtube.com/embed/${new URLSearchParams(
-                  new URL(freeVideos[0].video_url).search,
-                ).get('v')}`}
-                title={freeVideos[0].subject_name}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen></iframe>
+              {freeVideos[0].video_url ? (
+                // Проверка наличия ID видео
+                extractVideoId(freeVideos[0].video_url) ? (
+                  <iframe
+                    className="absolute top-0 left-0 w-full h-full rounded-xl border"
+                    src={`https://www.youtube.com/embed/${extractVideoId(freeVideos[0].video_url)}`}
+                    title={freeVideos[0].subject_name || 'Видео'}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+                ) : (
+                  <div>Некорректный URL видео</div>
+                )
+              ) : (
+                <div>Видео URL отсутствует</div>
+              )}
 
               <span className="absolute m-2 bottom-0 right-0 w-[154px] text-center cursor-pointer bg-[#4C4C4C] hover:bg-[#2E3095] text-white px-4 py-1 rounded-full z-10">
-                {freeVideos[0].subject_category_name}
+                {freeVideos[0].subject_category_name || 'Категория'}
               </span>
             </div>
 
             <h3 className="text-lg w-full lg:w-[450px] text-[#252641] md:text-xl font-semibold">
-              {freeVideos[0].subject_name}
+              {freeVideos[0].subject_name || 'Название урока'}
             </h3>
             <p className="text-gray-500 w-full lg:w-[470px] text-sm md:text-base">
-              {freeVideos[0].description}
+              {freeVideos[0].description || 'Описание отсутствует'}
             </p>
             <Link href="#" className="text-[#696984] underline font-semibold">
               Толук оку
             </Link>
           </div>
         )}
-
-        {/* <div className="space-y-0">
-          {freeVideos.slice(1).map((video) => (
-            <div
-              key={video.id}
-              className="flex flex-col sm:flex-row gap-4 items-center pb-2"
-            >
-              <div className="relative">
-                <iframe
-                  className="w-full h-auto sm:max-w-[600px] md:max-w-[600px] lg:w-[270px] max-h-[300px] rounded-xl border"
-                  src={`https://www.youtube.com/embed/${new URLSearchParams(
-                    new URL(video.video_url).search
-                  ).get("v")}`}
-                  title={video.subject_name}
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-                <span className="absolute text-[14px] w-[154px] text-center cursor-pointer pr-3 pl-3 z-10 right-2 bottom-0 bg-[#4C4C4C] hover:bg-[#2E3095] text-white px-2 py-1 rounded-full mb-2">
-                  {video.subject_category_name}
-                </span>
-              </div>
-              <div>
-                <h3 className="text-lg lg:w-[388px] text-[#252641] md:text-xl font-semibold">
-                  {video.subject_name}
-                </h3>
-                <p className="text-gray-500 lg:pt-5 lg:w-[300px] text-sm md:text-base">
-                  {video.description}
-                </p>
-                <Link
-                  href="#"
-                  className="sm:hidden text-[#696984] text-start mt-1 underline text-lg font-semibold flex justify-start"
-                >
-                  Толук окуу
-                </Link>
-              </div>
-            </div>
-          ))} */}
-        {/* </div> */}
       </div>
     </div>
   );
--- src/components/user/footer/Footer.tsx
+++ src/components/user/footer/Footer.tsx
@@ -1,37 +1,30 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import React from "react";
-import logo from "../../../../public/images/logo.svg";
-import { Mail, MapPin } from "lucide-react";
+import Image from 'next/image';
+import React from 'react';
+import logo from '../../../../public/images/logo.svg';
+import { Mail, MapPin } from 'lucide-react';
 
-import { usePathname } from "next/navigation";
+import { usePathname } from 'next/navigation';
 
 const Footer = () => {
   const pathName = usePathname();
-  const shouldRenderFooter = !pathName.startsWith("/in/");
+  const shouldRenderFooter = !pathName.startsWith('/in/');
   return (
     <div
       className={`first-line:w-[90%] mx-auto max-w-[1440px] px-[10px] py-[20px] ${
-        shouldRenderFooter ? "block" : "hidden"
-      }`}
-    >
+        shouldRenderFooter ? 'block' : 'hidden'
+      }`}>
       <div className="flex flex-col md:flex-row md: justify-between items-center">
         <div>
-          <Image
-            src={logo}
-            alt=""
-            width={82}
-            height={31}
-            className="md:w-[100px] lg:w-[170px]"
-          />
+          <Image src={logo} alt="" width={82} height={31} className="md:w-[100px] lg:w-[170px]" />
           <h1 className="text-[3.5px] text-center font-montserrat font-black text-[#2F2F99] lg:text-[6px]">
             Аналитикалык жана билим берүү уюму
           </h1>
         </div>
         <p className="w-[229px] text-[10px] md:text-[15px] md:w-[300px] lg:w-[448px] leading-[16px] lg:leading-[32px] text-center mt-[30px] text-[#4C4C4C] lg:text-[20px]">
-          ЖРТ абитуриенттин билим деңгээлин жана логикалык ой жүгүртүүсүн баалоо
-          максатында өткөрүлөт
+          ЖРТ абитуриенттин билим деңгээлин жана логикалык ой жүгүртүүсүн баалоо максатында
+          өткөрүлөт
         </p>
         <nav className="mt-[20px] flex gap-[17px] md:flex-col">
           <div className="flex gap-[9px] items-center md:gap-[15px]">
@@ -44,27 +37,24 @@ const Footer = () => {
             />
             <a
               href="tel:+9969999999"
-              className=" text-[9px]  text-[#4C4C4C] font-semibold md:text-[16px]"
-            >
-              (504) 186600
+              className=" text-[9px]  text-[#4C4C4C] font-semibold md:text-[16px]">
+              +996 997 44 07 99
             </a>
           </div>
           <div className="flex gap-[9px] items-center md:gap-[15px]">
             <Mail className="w-[16px] h-[16px] text-[#4C4C4C] md:w-[24px] md:h-[24px]" />
             <a
               href="mailto:Zhrt@gmail.com"
-              className="text-[9px] text-[#4C4C4C] font-semibold md:text-[16px]"
-            >
-              recordonline@gmail.com
+              className="text-[9px] text-[#4C4C4C] font-semibold md:text-[16px]">
+              record.onlline@gmail.com
             </a>
           </div>
           <div className="flex gap-[9px] items-center md:gap-[15px]">
             <MapPin className="w-[16px] h-[16px] text-[#4C4C4C] md:w-[24px] md:h-[24px]" />
             <a
               href="Location: Osh, Bishkek, Kyrgyzstan"
-              className=" text-[9px] text-[#4C4C4C] font-semibold md:text-[16px]"
-            >
-              720000 Osh
+              className=" text-[9px] text-[#4C4C4C] font-semibold md:text-[16px]">
+              720000 Бишкек
             </a>
           </div>
         </nav>
