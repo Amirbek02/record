9aab57f758bc5525d4fc2c57b7a656b7e0855f61
Amirbek02 <amirbek.amiraev.kk@gmail.com>
2025-01-29 01:56:00 +0600

    import mistakes fixed

--- src/components/user/UserIn/mainpage/MainPage.tsx
+++ src/components/user/UserIn/mainpage/MainPage.tsx
@@ -1,9 +1,46 @@
-import React from "react";
-import Banner from "./Banner";
-import MainTestCarousel from "./MainTestCarousel";
-import VideoLessonCarousel from "./VideoLessonCarousel";
+import React from 'react';
+import Banner from './Banner';
+import MainTestCarousel from './MainTestCarousel';
+import VideoLessonCarousel from './VideoLessonCarousel';
+import { useRouter } from 'next/navigation';
+import useAuthStore, { IVerificationResponse } from '@/lib/store/authStore';
+import { jwtDecode } from 'jwt-decode';
 
 const MainPage = () => {
+  const { verification } = useAuthStore();
+  const router = useRouter();
+
+  const loginWithToken = React.useCallback(
+    async (token: string) => {
+      try {
+        await verification({ token } as IVerificationResponse);
+        router.push('/in');
+      } catch (err) {
+        console.error('Error during token login:', err);
+      }
+    },
+    [verification, router],
+  );
+
+  React.useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const decoded = jwtDecode(token);
+        if (decoded.exp && decoded.exp * 1000 > Date.now()) {
+          loginWithToken(token);
+        } else {
+          console.warn('Token expired, redirecting...');
+          router.push('/sign-up');
+        }
+      } catch (error) {
+        console.error('Invalid token format:', error);
+        router.push('/sign-up');
+      }
+    } else {
+      router.push('/sign-up');
+    }
+  }, [loginWithToken, router]);
   return (
     <div className="flex flex-col gap-1">
       <Banner />
