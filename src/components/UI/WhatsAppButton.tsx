581d554546674edd66eb46dfdff1b3b638cb8660
Amirbek02 <amirbek.amiraev.kk@gmail.com>
2025-02-03 02:01:45 +0600

    added max-width of header

--- src/components/UI/WhatsAppButton.tsx
+++ src/components/UI/WhatsAppButton.tsx
@@ -1,9 +1,9 @@
-import { FaWhatsapp } from "react-icons/fa";
+import { FaWhatsapp } from 'react-icons/fa';
 
 export default function WhatsAppButton() {
-  const phoneNumber = "996997440799"; // Замени на свой номер
+  const phoneNumber = '996997440799'; // Замени на свой номер
   const message = encodeURIComponent(
-    "Саламатсызбы, мен recordonline.kg сайтынан курс боюнча маалымат алуу үчүн жазып жатам ..."
+    'Саламатсызбы, мен recordonline.kg сайтынан курс боюнча маалымат алуу үчүн жазып жатам ...',
   );
 
   return (
@@ -11,9 +11,8 @@ export default function WhatsAppButton() {
       href={`https://wa.me/${phoneNumber}?text=${message}`}
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-4 left-4 bg-green-600 text-white p-3 rounded-full shadow-lg hover:bg-green-700 transition animate-glow"
-      style={{ backgroundColor: "#218838" }}
-    >
+      className="fixed bottom-4 right-4 bg-green-600 text-white p-3 rounded-full shadow-xl hover:bg-green-700 transition animate-glow"
+      style={{ backgroundColor: '#218838' }}>
       <div className="relative flex items-center justify-center">
         <span className="absolute w-12 h-12 bg-green-500 opacity-50 rounded-full animate-ping"></span>
         <FaWhatsapp size={30} />
